bfc35312e5d447cd668bbb51a3ada2a89c23dcc4
Mattia Picchio <picchiomattia@gmail.com>
2024-11-12 16:54:22 +0100

    chore: install and configure tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div>Reddish</div>
+      <div className="text-orange">Reddish</div>
     </>
   );
 }
---
+++ src/assets/css/tailwindColors.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  gray: {
+    DEFAULT: "#878A8C",
+    dark: "#5A5A5A",
+    light: "#FF4500",
+  },
+  black: {
+    DEFAULT: "#000",
+    rich: "#040307",
+  },
+  current: "currentColor",
+  transparent: "transparent",
+  orange: "#FF4500",
+  blue: "#0079D3",
+};

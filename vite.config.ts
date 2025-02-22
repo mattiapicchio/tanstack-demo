ed68a2acd1e43be40dabba9a70628fabfd66f5f0
Mattia Picchio <picchiomattia@gmail.com>
2025-02-22 16:02:46 +0100

    build: change port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 
 const root = path.resolve(__dirname, "./src");
 
@@ -12,4 +12,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
 });

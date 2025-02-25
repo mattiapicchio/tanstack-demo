6bc035d15f7b3fad8cad3fb91199c23b936b6feb
Mattia Picchio <picchiomattia@gmail.com>
2025-02-25 18:53:47 +0100

    perf: change staleTime and gcTime

--- src/main.tsx
+++ src/main.tsx
@@ -10,13 +10,13 @@ import { routeTree } from "./routeTree.gen";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // The duration until a query transitions from fresh to stale. As long as the query is fresh, data will always be read from the cache only. With SSR, we usually want to set some default staleTime above 0 to avoid refetching immediately on the client. --> https://tanstack.com/query/v5/docs/framework/react/guides/caching
-      staleTime: minutes(1),
+      // The duration until a query transitions from fresh to stale. As long as the query is fresh, data will always be read from the cache only.
+      staleTime: minutes(5),
       // Queries that fail are silently retried 3 times, with exponential backoff delay
       retry: 3,
       refetchOnWindowFocus: false,
       // The duration until inactive queries will be removed from the cache and garbage collected.
-      gcTime: minutes(5),
+      gcTime: minutes(10),
     },
   },
 });
